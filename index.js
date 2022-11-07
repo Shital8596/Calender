6299e881b6c10be373b4b6801286daa6a47a1773
Shital8596 <101247710+Shital8596@users.noreply.github.com>
2022-11-07 12:27:33 +0530

    Update index.js

--- index.js
+++ index.js
@@ -6,21 +6,21 @@ function displayDefaultCalender() {
 
     const lastMonthDay = new Date(yea, mon + 1, 0).getDate();
     const firstMonDay = date.getDay() + 1;
-    const lastWeekDay = new Date(yea, mon + 1, 0).getDay();
+    const lastWeekDay = new Date(yea, mon + 1, 0).getDay() + 1;
     console.log(lastWeekDay)
     let j = 0;
     let monthDays = document.getElementById("tableRows")
-    let days = ""; 
+    let days = "";
 
     while (j < firstMonDay) {
         days += `<div class="grid-item"></div>`
         j++;
     }
-    
+
     for (let i = 1; i <= lastMonthDay; i++) {
-        days += `<div class="grid-item" id="${i}">${i}</div>`
+        days += `<div class="grid-item itemDiv" id="${i}">${i}</div>`
     }
-    for (let i = lastWeekDay+1; i <= 7; i++) {
+    for (let i = lastWeekDay; i < 7; i++) {
         days += `<div class="grid-item"></div>`
     }
     monthDays.innerHTML = days
@@ -31,9 +31,9 @@ displayDefaultCalender();
 
 function showCalender() {
     let month, year, lastDay, firstDay;
-    days = "";
     month = document.getElementById("selectMonth").value
     year = document.getElementById("selectYear").value
+
 
     async function getData() {
         let res = await fetch('http://localhost/calendar/calendar.php/', {
@@ -45,16 +45,21 @@ function showCalender() {
         console.log(data);
         firstDay = data.firstDay;
         lastDay = data.daysInMonth;
-        lastWeekDay = data.lastDay;
-        displayCalender(firstDay, lastDay, lastWeekDay);
+        // console.log(lastWeekDay);
+        displayCalender(firstDay, lastDay);
     }
 
-    // console.log(firstDay) 
-    // console.log(lastDay) 
-    function displayCalender(firstDay, lastDay, lastWeekDay) {
+
+    function displayCalender(firstDay, lastDay) {
+        const d = new Date(`${year}-${month}-${lastDay}`);
+        const yea = d.getFullYear();
+        const mon = d.getMonth();
+        const lastWeekDay = new Date(yea, mon + 1, 0).getDay() + 1;
+
+
         let j = 0;
         let monthDays = document.getElementById("tableRows")
-        // let days = ""; 
+        let days = "";
 
         while (j < firstDay) {
             days += `<div class="grid-item"></div>`
@@ -62,9 +67,9 @@ function showCalender() {
         }
 
         for (let i = 1; i <= lastDay; i++) {
-            days += `<div class="grid-item" id="${i}">${i}</div>`
+            days += `<div class="grid-item itemDiv" id="${i}">${i}</div>`
         }
-        for (let i = lastWeekDay + 1; i <= 7; i++) {
+        for (let i = lastWeekDay; i <= 7; i++) {
             days += `<div class="grid-item"></div>`
         }
         monthDays.innerHTML = days
@@ -72,44 +77,10 @@ function showCalender() {
     getData()
 }
 
-const currentCells = document.querySelectorAll(".grid-item");
-const enterDate = document.getElementById("enterBtn");
-const gridCell = document.getElementById("tableRows");
-// const dateVal = document.getElementById("inputText").value;
-
-// gridCell.addEventListener('click', (e) => {
-//     let id = e.target.id;
-
-//     enterDate.addEventListener('click', () => {
-//         const dateVal = document.getElementById("inputText").value;
-//         console.log(dateVal)
-//         async function getDataForBg() {
-//             let res = await fetch('http://localhost/calendar/colorBg.php/', {
-//                 method: 'POST', headers: {
-//                     'Content-Type': 'application/json',
-//                 }, body: JSON.stringify({ "date": dateVal })
-//             })
-//             let data = await res.json();
-//             console.log(data);
-//             color = data.color;
-//             setColor(color);
-//         }
-//         getDataForBg()
-
-//         let curCell = document.getElementById(id);
-
-//         function setColor(color) {
-//             if (color == "green") {
-//                 curCell.style.backgroundColor = "green";
-//             } else {
-//                 curCell.style.backgroundColor = "white";
-//             }
-//         }
-//     })
-// })
 
 
 
+// Function to change bgColor
 
 function changeBg() {
 
@@ -124,37 +95,23 @@ function changeBg() {
         let data = await res.json();
         console.log(data);
         color = data.color;
-        selectedList = data.selectedDates;
-        console.log(selectedList);
-        setColor(color, selectedList);
+        setColor(color);
     }
 
-    function setColor(color, array) {
-        const currentCells = document.querySelectorAll(".grid-item");
-        for (let item of currentCells) {
-            // console.log(item.id)
-            array.map(cell => {
-                if(cell){
-                    if (item.id == cell) {
-                        item.style.backgroundColor = "green";
-                    }
+    const currentCells = document.querySelectorAll(".itemDiv");
+    for (let item of currentCells) {
+        // console.log(item.id)
+        if (item.id == dateVal) {
+            function setColor(color) {
+                if (color == "green") {
+                    item.style.backgroundColor = "green";
+                } else {
+                    item.style.backgroundColor = "white";
                 }
-            })
+            }
         }
     }
-    // const currentCells = document.querySelectorAll(".grid-item");
-    // for (let item of currentCells) {
-    //     // console.log(item.id)
-    //     if (item.id == dateVal) {
-    //         function setColor(color) {
-    //             if (color == "green") {
-    //                 item.style.backgroundColor = "green";
-    //             } else {
-    //                 item.style.backgroundColor = "white";
-    //             }
-    //         }
-    //     }
-    // }
+
     getDataForBg()
 }
 
